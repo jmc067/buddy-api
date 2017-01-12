d6024c1578305898d4484c6174ec0e501478c745
Julian <julian@iris.tv>
2017-01-11 16:05:48 -0800

    added all possible item fields

--- MenuItem.rb
+++ MenuItem.rb
@@ -4,23 +4,44 @@ require_relative './Database'
 class MenuItem
 	@@db ||= Database.new()
 
-	attr_accessor :name, :code
+	attr_accessor :name, :code, :name, :category, :type, :thc_percent, :thc_g, :description, :image, :gram_price, :eighth_price, :quarter_price, :half_price, :oz_price
 
 	def initialize(hash)
-		@name = ""
 		@code = BSON::ObjectId.new.to_s
 		self.update(hash)
 	end
 
 	def update(hash)
-		@name = hash["name"].to_s if hash["name"] 
-		@code = hash["code"].to_s if hash["code"] 
+		@name = hash["name"].to_s if hash["name"]
+		@code = hash["code"].to_s if hash["code"]
+		@category = hash["category"].to_s if hash["category"]
+		@type = hash["type"].to_s if hash["type"]
+		@thc_percent = hash["thc_percent"].to_i if hash["thc_percent"]
+		@thc_g = hash["thc_g"].to_i if hash["thc_g"]
+		@description = hash["description"].to_s if hash["description"]
+		@image = hash["image"].to_s if hash["image"]
+		@gram_price = hash["gram_price"].to_i if hash["gram_price"]
+		@eighth_price = hash["eighth_price"].to_i if hash["eighth_price"]
+		@quarter_price = hash["quarter_price"].to_i if hash["quarter_price"]
+		@half_price = hash["half_price"].to_i if hash["half_price"]
+		@oz_price = hash["oz_price"].to_i if hash["oz_price"]
 	end
 
 	def format()
 		menu = {
-			"name"=>@name,
-			"code"=>@code
+			"name" => @name,
+			"code" => @code,
+			"category" => @category,
+			"type" => @type,
+			"thc_percent" => @thc_percent,
+			"thc_g" => @thc_g,
+			"description" => @description,
+			"image" => @image,
+			"gram_price" => @gram_price,
+			"eighth_price" => @eighth_price,
+			"quarter_price" => @quarter_price,
+			"half_price" => @half_price,
+			"oz_price" => @oz_price
 		}
 		return menu
 	end
